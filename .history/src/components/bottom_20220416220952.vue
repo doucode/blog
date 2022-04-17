d82fbdd3e8feefc134ae7a360aee1b97a9701d3b
DouCode <=>
2022-04-17 16:14:25 +0800

    blog

---
+++ .history/src/components/bottom_20220416220952.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="bottom">
+    <div class="line"></div>
+    <p id="txt">
+      <el-link
+        :underline="false"
+        type="info"
+      >© 2022 Dou&nbsp;&nbsp;</el-link>
+    </p>
+
+    <div class="line" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bottom'
+}
+</script>
+<style scoped>
+.line {
+  width: 100%;
+  height: 4px;
+}
+
+#bottom {
+  color: #303133;
+  background: #f2f6fc;
+  margin-top: 90px;
+}
+
+.sdfsdf {
+  color: #8c939d;
+}
+
+.contact-qq {
+  width: 20px;
+  height: 20px;
+}
+</style>
